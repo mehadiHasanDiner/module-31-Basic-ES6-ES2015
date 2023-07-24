8d3314833839b95d60f4bb7df38bcf0e229182f2
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-25 02:59:49 +0600

    video-6

---
+++ arrow.js
@@ -0,0 +1,11 @@
+const add6 = (first, second) => first + second;
+
+const result = add6(15, 20);
+console.log(result);
+
+// () = এটার মানে হচ্ছে Function
+// => এটার মানে হচ্ছে Return
+
+// () => এটা হচ্ছে ‍arrow function
+
+// Interview question: difference between function declarations and  function expressions ard arrow function
---
+++ template.js
@@ -0,0 +1,14 @@
+const neWMultiLine = `First line of Text
+second line of Text
+third line of Text
+fourth line of Text
+`;
+// console.log(neWMultiLine);
+
+const a = 20;
+const b = 34;
+const num = [1, 2, 3, 4, 5];
+const newSummary = `sum of ${a} and ${b} is ${a + b}`;
+const newSummaryArray = `sum of ${a} and ${num.length} is ${a + b}`;
+
+console.log(newSummaryArray);
